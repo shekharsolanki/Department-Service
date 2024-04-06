cef86cb3b4f2f1125b03329c624e5d96bd36be87
shekharsolanki <er.shekharsolanki@gmail.com>
2024-04-06 23:55:57 +0530

    Department-Service Eighth Commit and Push

--- src/main/java/in/raghavmadhav/Department/Service/DepartmentServiceApplication.java
+++ src/main/java/in/raghavmadhav/Department/Service/DepartmentServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentServiceApplication.class, args);
+    }
 }
